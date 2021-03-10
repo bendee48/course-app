b9064a6ad886bf5d4d3d167cf25c7a792bbd1b93
Ben Daniels <benbanj@hotmail.com>
2021-03-10 15:02:01 +0000

    Styling home/show/edit pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:landing_page]
+
   def landing_page
+    @latest_courses = Course.order(created_at: :desc).three
+    @courses = Course.three
   end
 
   def privacy_policy
--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,8 @@ class Course < ApplicationRecord
   has_rich_text :description
   belongs_to :user
 
+  scope :three, -> { limit(3) }
+
   # Shows course object as title
   def to_s
     title
