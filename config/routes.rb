dd8522402ade3006e0ce1f1fd9c07581b39000e4
Ben Daniels <benbanj@hotmail.com>
2021-03-12 16:01:30 +0000

    Adding Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   devise_for :users
+  resources :users, only: [:index]
   resources :courses
 end
